197733831d32db263973ddb637c64b8485bae516
Joe Gallagher <github@pingmint.net>
2025-12-28 12:15:08 -0500

    setup vite

---
+++ LICENSE.md
@@ -0,0 +1,3 @@
+Copyright (c) 2026, Joe Gallagher
+
+All rights reserved.
---
+++ src/extension/web/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [_count, _setCount] = useState(0)
+
+  return (
+    <>
+      <p className="read-the-docs">
+        Hi!
+      </p>
+    </>
+  )
+}
+
+export default App
